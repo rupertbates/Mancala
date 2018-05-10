d9862b83f1671cdd8b771001abcb6311d9a2ca72
rupertbates <rupert.bates@guardian.co.uk>
2018-05-10 12:07:48 +0100

    Board sorted

--- src/Board.js
+++ src/Board.js
@@ -6,48 +6,68 @@ class Board extends Component {
 
     constructor(props) {
         super(props);
-        const x = 30;
-        const y = 50;
+        const x = 150;
+        const y = 150;
         const hSpacing = 15;
-        const vSpacing = 15;
+        const vSpacing = 25;
+        const boardHMargin = 6 * hSpacing;
         const boxWidth = 100;
+        const boxHeight = 120;
+        const dishWidth = 100;
+        const dishHeight = (2 * boxHeight) + vSpacing;
+        const dish1Left = x + boardHMargin;
+        const boxesLeft = dish1Left + dishWidth + hSpacing;
         this.state = {
             x,
             y,
             width: 300,
             height: 200,
-            player1Cells: [...Array(6).keys()],
-            player2Cells: [...Array(6).keys()],
-            dishes: [...Array(2).keys()],
-            boxHeight: 80,
-            boxWidth,
             hSpacing,
-            vSpacing: 15,
-            cellsLeft: x + boxWidth + (2 * hSpacing) // space for the dish on the left side
+            vSpacing,
+            boardHMargin,
+            player1Boxes: [...Array(6).keys()],
+            player2Boxes: [...Array(6).keys()],
+            dishes: [...Array(2).keys()],
+            boxWidth,
+            boxHeight,
+            boxColour: '#ced130',
+            dishColour: '#ced130',
+            backgroundColour: '#1d6a82',
+            dishWidth,
+            dishHeight,
+            dish1Left,
+            dish2Left: boxesLeft + (6 * boxWidth) + (6 * hSpacing),
+            boxesLeft: dish1Left + dishWidth + hSpacing, // space for the dish on the left side
+            boxesTop: y + vSpacing,
         };
     }
 
     render() {
-        const player1Boxes = this.state.player1Cells.map(i => this.generateBoxes(this.state.y, i));
-        const player2Boxes = this.state.player2Cells.map(i => this.generateBoxes((this.state.y + this.state.boxHeight + this.state.vSpacing), i));
+        const player1Boxes = this.state.player1Boxes.map(i => this.generateBoxes(this.state.boxesTop, i));
+        const player2Boxes = this.state.player2Boxes.map(i => this.generateBoxes((this.state.boxesTop + this.state.boxHeight + this.state.vSpacing), i));
 
         return (
-            <Group
-                x={this.state.x}
-                y={this.state.y}
-                width={this.state.width}
-                height={this.state.height}
-                visible={true}
-            >
+            <Group>
                 <Background {... this.state} />
                 {player1Boxes}
                 {player2Boxes}
-                <Dish x={this.state.x + this.state.hSpacing} y={this.state.y} width={this.state.boxWidth} height={this.state.height - (2 * this.state.vSpacing)} />
+                <Dish
+                    x={this.state.dish1Left}
+                    y={this.state.boxesTop}
+                    width={this.state.dishWidth}
+                    height={this.state.dishHeight}
+                    colour={this.state.dishColour} />
+                <Dish
+                    x={this.state.dish2Left}
+                    y={this.state.boxesTop}
+                    width={this.state.dishWidth}
+                    height={this.state.dishHeight}
+                    colour={this.state.dishColour} />
             </Group>
         );
     }
 
-    
+
 
     generateBoxes(y, i) {
         return (<Rect
@@ -57,30 +77,30 @@ class Board extends Component {
             width={this.state.boxWidth}
             height={this.state.boxHeight}
             cornerRadius={this.state.boxWidth / 2}
-            fill={'#aaa'}
+            fill={this.state.boxColour}
         />)
     }
 
     getX(i) {
-        return this.state.cellsLeft + (i * this.state.boxWidth) + (i * this.state.hSpacing);
+        return this.state.boxesLeft + (i * this.state.boxWidth) + (i * this.state.hSpacing);
     }
 }
 
 function Background(props) {
-    const width = 8 * (props.boxWidth + props.hSpacing)
-    const height = 2.3 * (props.boxHeight + props.vSpacing)
+    const width = (props.boxWidth * 6) + (props.hSpacing * 7) + (props.dishWidth * 2) + (props.boardHMargin * 2);
+    const height = (2 * props.boxHeight) + (3 * props.vSpacing);
     return (
-        <Rect 
+        <Rect
             x={props.x}
-            y={props.y - props.vSpacing}
-            cornerRadius={100}
+            y={props.y}
+            cornerRadius={width / 3}
             width={width}
             height={height}
-            fill={'#444'}
+            fill={props.backgroundColour}
         />
     );
 }
-function Dish(props)  {
+function Dish(props) {
     return (
         <Rect
             x={props.x}
@@ -88,7 +108,7 @@ function Dish(props)  {
             width={props.width}
             height={props.height}
             cornerRadius={props.height / 2}
-            fill={'#aaa'}
+            fill={props.colour}
         />
     )
 }
