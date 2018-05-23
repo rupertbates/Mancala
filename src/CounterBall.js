0ee613b3f96b78392272d0b64185a5b2d35eddbd
rupertbates <rupert.bates@guardian.co.uk>
2018-05-23 17:54:59 +0100

    Fix the positioning in the animations - still wrong after finishing in a dish

--- src/CounterBall.js
+++ src/CounterBall.js
@@ -4,12 +4,12 @@ export default function CounterBall(props) {
     const hidden = props.counters === 0 ? 'hidden' : '';
     const hoverClass = props.hover ? 'ball-hover' : '';
     return (
-        <div id={`ball-${props.gameIndex}`} className={`${props.classModifier} ball-wrapper ${hidden}`}>
+        <div id={`ball-${props.gameIndex}`} className={`ball-wrapper ${hidden}`}>
             <section className="stage">
-            <figure className={`ball ${hoverClass}`}>
-                <span className="shadow" />
-                <span className="counter-text">{props.counters.toString()}</span>
-            </figure>
+                <figure className={`ball ${hoverClass}`}>
+                    <span className="shadow" />
+                    <span className="counter-text">{props.counters.toString()}</span>
+                </figure>
             </section>
         </div>
     )
--- src/animations.js
+++ src/animations.js
@@ -1,46 +1,53 @@
-import { TimelineLite, TweenLite, Power2 } from 'gsap';
+import { TimelineLite, TweenLite, Power2 } from 'gsap'
 
-function moveFirstCounter(fromIndex, toIndex, callback) {
+
+function moveFirstCounter (fromIndex, toIndex, callback) {
     const from = document.getElementById(`ball-${fromIndex}`)
-    const fromRect = from.getBoundingClientRect();
+    const fromRect = from.getBoundingClientRect()
     const to = document.getElementById(`ball-${toIndex}`)
-    const toRect = to.getBoundingClientRect();
+    const toRect = to.getBoundingClientRect()
 
-    from.style.zIndex = 2;
-    to.style.zIndex = 1;
+    from.style.zIndex = '2'
+
+    const onComplete = () => {
+        from.style.cssText = ''
+        callback()
+    }
 
     TweenLite.to(from, 0.5,
         {
             z: 2,
             x: (toRect.left - fromRect.left),
             y: (toRect.top - fromRect.top),
-            onComplete: callback,
-        });
+            onComplete,
+        })
 }
 
 function moveNextCounter (fromIndex, toIndex, callback) {
-    const moving = document.getElementById("ball-99")
     const from = document.getElementById(`ball-${fromIndex}`)
-    const fromRect = from.getBoundingClientRect();
+    const fromRect = from.getBoundingClientRect()
     const to = document.getElementById(`ball-${toIndex}`)
-    const toRect = to.getBoundingClientRect();
-
-    from.style.zIndex = 2;
-    to.style.zIndex = 1;
+    const toRect = to.getBoundingClientRect()
 
     //Here we animate a separate ball, not the original one which was clicked
-    moving.style.top = '67px'
-
     TweenLite.fromTo('#ball-99', 0.5,
         {
             x: fromRect.x,
-            y: fromRect.y,
+            y: fromRect.y - getOffset(toRect, fromRect),
         },
         {
-            x: (toRect.left),
-            y: (toRect.top),
+            x: toRect.left,
+            y: toRect.top - getOffset(fromRect, toRect),
             onComplete: callback,
-        });
+        })
+}
+
+//This is to account for the difference in height between dishes and boxes
+function getOffset (fromRect, toRect) {
+    if (fromRect.height < toRect.height)
+        return (fromRect.height - toRect.height) / 2
+
+    return 0;
 }
 
 export {
